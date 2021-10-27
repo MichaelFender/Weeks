59e4b982d0953d848094c66b3c2957f105171d35
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-27 16:44:46 -0500

    Next create a new span element and save it to a variable called movieTitle. Set the textContent of movieTitle to be the value of inputField.

--- Week3/js-interactivity/index.js
+++ Week3/js-interactivity/index.js
@@ -2,5 +2,10 @@ let addMovie = (event) => {
     let inputField = document.querySelector("input"); 
 
     let movie = document.createElement('Li');
-}
+
+    let movieTitle = document.createElement('span');
+
+    movieTitle.textContent(inputField.value);
+
+};
 
