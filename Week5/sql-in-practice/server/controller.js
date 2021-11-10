3522957443e40153a379f299f14d03241ee49f70
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-10 14:55:04 -0600

    not seeing client information . time to debug

--- Week5/sql-in-practice/server/controller.js
+++ Week5/sql-in-practice/server/controller.js
@@ -1,3 +1,18 @@
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const { CONNECTION_STRING } = process.env;
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+});
+
 let nextEmp = 5
 
 module.exports = {
@@ -27,5 +42,16 @@ module.exports = {
                 nextEmp += 2
             })
             .catch(err => console.log(err))
-    }
+    },
+    getAllClients: (req, res) => {
+        sequelize
+            .query(
+                `
+            select * from cc_clients c
+            join cc_users u on c.user_id = u.user_id
+            where u.user_id = ${userId};`
+            )
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log(err));
+    },
 }
