8783fa301af53a61a20450a101d948630db0aa95
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-10 13:33:04 -0600

    syntax corrections

--- Week5/sql-in-practice-demo/server/controller.js
+++ Week5/sql-in-practice-demo/server/controller.js
@@ -1,14 +1,31 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize");
 
-const { CONNECTION_STRING } = process.env
+const { CONNECTION_STRING } = process.env;
 
 const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: 'postgres',
+    dialect: "postgres",
     dialectOptions: {
         ssl: {
-            rejectUnauthorized: false
+            rejectUnauthorized: false,
         },
     },
 });
+
+const userId = 4;
+const clientId = 3;
+
+module.exports = {
+    getUserInfo: (req, res) => {
+        sequelize
+            .query(
+                `
+            select * from cc_clients c
+            join cc_users u on c.user_id = u.user_id
+            where u.user_id = ${userId};`
+            )
+            .then((dbRes) => res.status(200).send(dbRes[0]))
+            .catch((err) => console.log(err));
+    },
+};
