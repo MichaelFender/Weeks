b1e1c99cb5a03f32eb28d6abb16f88918440c967
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-11 16:27:13 -0600

    WTF

--- Week5/sql-in-practice/server/controller.js
+++ Week5/sql-in-practice/server/controller.js
@@ -1,26 +1,54 @@
-require("dotenv").config();
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
 
-const Sequelize = require("sequelize");
-
-const { CONNECTION_STRING } = process.env;
-
+// you wouldn't want to rejectUnauthorized in a production app, but it's great for practice
 const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: "postgres",
+    dialect: 'postgres', 
     dialectOptions: {
         ssl: {
-            rejectUnauthorized: false,
-        },
-    },
-});
+            rejectUnauthorized: false
+        }
+    }
+})
 
 let nextEmp = 5
 
 module.exports = {
+    getAllClients: (req, res) => {
+        sequelize.query(`select * from cc_clients c
+        join cc_users u on c.user_id = u.user_id;`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    }, 
+    
+    getPendingAppointments: (req, res) => {
+        sequelize.query(`select * from cc_appointments
+        where approved = false
+        order by date desc;`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    },
+
     getUpcomingAppointments: (req, res) => {
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = false
+        order by a.date desc;`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+    },
+
+    getPastAppointments: (req, res) => {
         sequelize.query(`select a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name 
         from cc_appointments a
-        join cc_users u on a.client_id = u.user_id
-        WHERE a.approved = true AND a.completed = false
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
         order by a.date desc;`)
             .then(dbRes => res.status(200).send(dbRes[0]))
             .catch(err => console.log(err))
@@ -28,8 +56,9 @@ module.exports = {
 
     approveAppointment: (req, res) => {
         let {apptId} = req.body
-    
-        sequelize.query(`*****YOUR CODE HERE*****
+
+        sequelize.query(`update cc_appointments set approved = true
+        where appt_id = ${apptId};
         
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
@@ -41,30 +70,13 @@ module.exports = {
             })
             .catch(err => console.log(err))
     },
-    getAllClients: (req, res) => {
-        sequelize
-            .query(
-                `
-            select * from cc_clients c
-            join cc_users u on c.user_id = u.user_id`
-            
-            )
-            .then((dbRes) => res.status(200).send(dbRes[0]))
-            .catch((err) => console.log(err));
-    },
-    
 
-    getPendingAppointments: (req, res) => {
-        sequelize
-        .query(`SELECT * FROM cc_appointments WHERE approved = false ORDER BY cc_appointments.date`)
-        
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+
+        sequelize.query(`update cc_appointments set completed = true
+        where appt_id = ${apptId};`)
             .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log('getPendingAppointments error', err))
-    },
-    getPastAppointments: (req, res) => {
-        sequelize.query(`SELECT * FROM cc_appointments;`)  
-        
-            .then(dbRes => res.status(200).send(dbRes[0]))
-            .catch(err => console.log('getPastAppointments error', err))
-    },
+            .catch(err => console.log(err))
+    }
 }
